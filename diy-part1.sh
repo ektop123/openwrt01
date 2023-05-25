67da342d07c6d1f36e72d29ae2d33ed6d02cc463
ektop123 <130059697+ektop123@users.noreply.github.com>
2023-05-25 20:37:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.gitl' >>feeds.conf.default
